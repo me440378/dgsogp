825177409c66c668b7d6fcf3998cdf8e3a5d7017
何浩嵘 <qq872559403@163.com>
2020-03-10 00:37:16 +0800

    对数据接口的数据模型进行了数据库迁移

--- dgsogp_django/backend/models/DatainterfacesModel.py
+++ dgsogp_django/backend/models/DatainterfacesModel.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Datainterfaces(models.Model):
-	id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     type = models.IntegerField()
     name = models.CharField(max_length=255)
     metadata_id = models.IntegerField()
